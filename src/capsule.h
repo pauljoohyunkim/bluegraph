296d7818466147ce2b3c248befb09b0f33cc8526
Paul Kim <pauljoohyunkim@gmail.com>
2025-06-15 00:31:14 +0200

    Query reply capsule type

--- src/capsule.h
+++ src/capsule.h
@@ -8,7 +8,8 @@ typedef enum
 {
     BLUEGRAPH_CAPUSLE_TYPE_SEND_MESSAGE,    // Sending message
     BLUEGRAPH_CAPSULE_TYPE_SEND_RECEIPT,    // Acknowledgment of receiving message
-    BLUEGRAPH_CAPSULE_QUERY                 // Querying if node can reach destination.
+    BLUEGRAPH_CAPSULE_TYPE_QUERY,           // Querying if node can reach destination.
+    BLUEGRAPH_CAPSULE_TYPE_QUERY_REPLY      // Reply to the query
 } CapsuleType;
 
 typedef struct
@@ -26,6 +27,16 @@ typedef struct
             // Implement this
         } send_receipt_info;
         // Implement query
+        struct
+        {
+            // Implement this
+        } query_info;
+        struct
+        {
+            // Implement this
+        } query_reply_info;
+        
+        
     };
 } Capsule_st;
 
