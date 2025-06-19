c8f5e1dc727b7ae3e6e602228758b809bbcc6a8e
Paul Kim <pauljoohyunkim@gmail.com>
2025-06-19 13:32:16 +0200

    Reading directory list

--- src/storage.c
+++ src/storage.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <dirent.h>
 #include "storage.h"
 
 BluegraphStorage bluegraph_load_storage()
@@ -9,7 +10,9 @@ BluegraphStorage bluegraph_load_storage()
     BluegraphStorage storage = NULL;
     char *homedir = NULL;
     const char bluegraphdir[] = "/.bluegraph";
-    struct stat st;
+    struct stat st = { 0 };
+    DIR *dp = NULL;
+    struct dirent *op = NULL;
 
     homedir = getenv("HOME");       // TODO: Make this cross-platform.
     if (!homedir)
@@ -36,6 +39,27 @@ BluegraphStorage bluegraph_load_storage()
         fprintf(stderr, "Creating %s\n", storage->dir);
         mkdir(storage->dir, 0700);
     }
+
+    // Read through the bluegraph rootdir
+    dp = opendir(storage->dir);
+    while ((op = readdir(dp)) != NULL)
+    {
+        struct stat filestat = { 0 };
+        char *filename = NULL;
+        if (strcmp(op->d_name, ".") == 0 || strcmp(op->d_name, "..") == 0)
+            continue;
+        filename = calloc(strlen(op->d_name) + strlen(storage->dir) + 2, sizeof(char));
+        strcpy(filename, storage->dir);
+        strcat(filename, "/");
+        strcat(filename, op->d_name);
+        stat(filename, &filestat);
+        if (!S_ISDIR(filestat.st_mode))
+            continue;
+        printf("%s\n", filename);
+        free(filename);
+    }
+    closedir(dp);
+    
     return storage;
 }
 
